4d1e588fdcf5618c58e2e8b393a6408b2440d576
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-14 09:55:51 +0300

    Create acclerate and break methods on the Car.prototype

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ const Car = function (make, speed) {
   this.make = make;
   this.speed = speed;
 };
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+Car.prototype.break = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
